f3a32e7314ce8eaca00ef59115f9841a8bcf41e4
Christopher Wright <22761542+xtophyr@users.noreply.github.com>
2024-05-07 14:05:24 -0400

    notes on GFNodeManagerView - very old patcherator :)

--- include/GFNodeManagerView.h
+++ include/GFNodeManagerView.h
@@ -1,3 +1,7 @@
+/* This view is roughly the Tiger-era patch creator - at least, the list of patches and description blurb at the bottom.
+    it supports drag-n-drop into a GFGraphEditorView, and appears to have some search/filtering capabilities too.
+ */
+
 extern NSString * const GFNodeManagerViewInstantiateNodeNotification;
 
 @interface GFNodeManagerView : NSView
@@ -12,32 +16,32 @@ extern NSString * const GFNodeManagerViewInstantiateNodeNotification;
 	void *_unused[4];	// 108 = 0x6c
 }
 
-- (id)initWithFrame:(NSRect)fp8;
+- (id)initWithFrame:(NSRect)frame;
 - (void)encodeWithCoder:(NSCoder *)aCoder;
 - (id)initWithCoder:(NSCoder *)aDecoder;
 - (void)dealloc;
-- (void)setNodeNamespace:(id)fp8;
+- (void)setNodeNamespace:(NSString*)nodeNamespace;   // set to @"com.apple.QuartzComposer" for a trip down memory lane
 - (id)nodeNamespace;
-- (void)setFilterString:(id)fp8;
+- (void)setFilterString:(NSString*)filterString;
 - (id)filterString;
 @end
 
 @interface GFNodeManagerView (TableViewSource)
-- (NSInteger)numberOfRowsInTableView:(NSTableView*)fp8;
-- (id)tableView:(NSTableView*)fp8 objectValueForTableColumn:(NSTableColumn*)fp12 row:(NSInteger)fp16;
-- (void)tableView:(NSTableView*)fp8 sortDescriptorsDidChange:(NSArray*)fp12;
-- (BOOL)tableView:(NSTableView*)fp8 writeRows:(NSArray*)fp12 toPasteboard:(NSPasteboard*)fp16;
+- (NSInteger)numberOfRowsInTableView:(NSTableView*)tableView;
+- (id)tableView:(NSTableView*)tableView objectValueForTableColumn:(NSTableColumn*)column row:(NSInteger)row;
+- (void)tableView:(NSTableView*)tableView sortDescriptorsDidChange:(NSArray*)fp12;
+- (BOOL)tableView:(NSTableView*)tableView writeRows:(NSArray*)fp12 toPasteboard:(NSPasteboard*)pboard;
 - (void)tableViewSelectionDidChange:(id)fp8;
 @end
 
 @interface GFNodeManagerView (Private)
 - (void)_finishInitialization;
 - (id)_descriptionForNode:(id)fp8;
-- (id)_cellContentForNode:(id)fp8 columnIdentifier:(id)fp12;
-- (int)_compareNode:(id)fp8 withNode:(id)fp12 usingColumnIdentifier:(id)fp16 ascending:(BOOL)fp20;
+- (id)_cellContentForNode:(id)fp8 columnIdentifier:(id)identifier;
+- (int)_compareNode:(id)fp8 withNode:(id)fp12 usingColumnIdentifier:(id)identifier ascending:(BOOL)flag;
 - (id)_filterStringsForNode:(id)fp8;
 - (void)_refreshList:(id)fp8;
-- (void)_addColumnWithIdentifier:(id)fp8 title:(id)fp12 dataCell:(id)fp16 sortable:(BOOL)fp20;
+- (void)_addColumnWithIdentifier:(id)fp8 title:(id)fp12 dataCell:(id)fp16 sortable:(BOOL)flag;
 - (void)_removeColumnWithIdentifier:(id)fp8;
 - (void)_removeAllColumns;
 - (id)_columns;

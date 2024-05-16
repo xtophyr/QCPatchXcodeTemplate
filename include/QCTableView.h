2181ef4707877a841cea16d336e1b38fa8c49ac0
Christopher Wright <22761542+xtophyr@users.noreply.github.com>
2024-05-16 16:54:31 -0400

    brief notes on QCTableView

--- include/QCTableView.h
+++ include/QCTableView.h
@@ -1,3 +1,5 @@
+// QCTableView is used as a superclass for a few other tableview-based inspectors (GL Info, for example).  It handles basic editing and cancellation.
+
 @interface QCTableView : NSTableView
 {
 	BOOL _cancelEditing;	// 228 = 0xe4

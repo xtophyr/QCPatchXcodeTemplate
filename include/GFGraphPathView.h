660c9d2da1bbf70a4b3cad3040a8356da551e43d
Christopher Wright <22761542+xtophyr@users.noreply.github.com>
2024-05-20 11:01:19 -0400

    remove a no-longer-present iVar, rearrange the methods to be a bit more sensible, add some types and names, and a couple notes.

--- include/GFGraphPathView.h
+++ include/GFGraphPathView.h
@@ -4,42 +4,46 @@
 // a nib it'll deserialize and be broken unless you drive _finishInitializing yourself.  This view is apparently only intended to be made
 // programaticaly.
 
+// a path in this context is similar to a file system path, where each "directory" is a macro patch.
+
 @class GFGraph, GFGraphEditorView, GFNode;
 
 @interface GFGraphPathView : NSView
 {
-	GFGraph *_rootGraph;	// 80 = 0x50
+	GFGraph *_rootGraph;	// 80 = 0x50        // this holds the root macro patch of the composition
 	GFGraphEditorView *_graphEditor;	// 84 = 0x54
-	GFNode *_currentNode;	// 88 = 0x58
-	BOOL _displayNodes;	// 92 = 0x5c
-	NSPathControl *_control;	// 96 = 0x60
+	GFNode *_currentNode;	// 88 = 0x58        // this holds the currently visible node in _graphEditor
+	NSPathControl *_control;	// 96 = 0x60    // draws the path from _rootGraph to _currentNode
 	void *_unused[4];	// 100 = 0x64
 }
 
-- (id)nodes;
-- (void)setRootGraph:(id)fp8;
-- (id)rootGraph;
-- (id)currentNode;
-- (void)__graphDidChange:(id)fp8;
-- (void)setPath:(NSString*)path;
-- (void)setCurrentNode:(id)fp8;
-- (void)setGraphEditor:(id)fp8;
-- (id)graphEditor;
-- (id)initWithFrame:(NSRect)fp8;
+- (id)initWithFrame:(NSRect)frame;
 - (void)dealloc;
+
+- (id)nodes;
+- (void)setRootGraph:(GFGraph*)graph;
+- (GFGraph*)rootGraph;
+- (void)setCurrentNode:(GFNode*)currentNode;
+- (GFNode*)currentNode;
+- (void)setGraphEditor:(GFGraphEditorView*)graphEditor;
+- (GFGraphEditorView*)graphEditor;
+
+- (void)__graphDidChange:(NSNotification*)notification;
+- (void)setPath:(NSString*)path;
 - (BOOL)isOpaque;
-- (void)drawRect:(NSRect)fp8;
+- (void)drawRect:(NSRect)rect;
 
 @end
 
 @interface GFGraphPathView (Private)
-+ (BOOL)_isGraph:(id)fp8;
++ (BOOL)_isGraph:(id)graph; // this checks graph if it's a GFGraph subclass.  used in various methods while traversing graphs to build/set paths.
 //- (BOOL)_getPathFromGraph:(id)fp8 toNode:(id)fp12 withBuffer:(id *)fp16; // this is now gone, replaced with the array variant
-- (BOOL)_getPathFromGraph:(id)graph toNode:(id)node withArray:(id)arg3; // added after SSDK was dumped
-- (void)_setCurrentNode:(id)node;
+- (BOOL)_getPathFromGraph:(id)graph toNode:(id)node withArray:(id)pathArray; // added after SSDK was dumped
 //- (id)_setPathWithBuffer:(id *)fp8; // this is now gone, replaced with the array variant
 - (id)_setPathWithArray:(NSArray*)pathArray;
+
+- (void)_setCurrentNode:(id)node;
 - (void)_graphUpdated:(NSNotification*)notification;
-- (void)__hierarchyControlAction:(id)fp8;
+- (void)__hierarchyControlAction:(id)sender;
 - (void)_finishInitialization;
 @end

c53378f95fb9ad4de6cd736e047870d7732aa0a5
Christopher Wright <22761542+xtophyr@users.noreply.github.com>
2024-05-19 21:18:37 -0400

    notes and a few types for GFGraphPathView.  A couple methods have changed slightly.

--- include/GFGraphPathView.h
+++ include/GFGraphPathView.h
@@ -1,4 +1,8 @@
 
+// GFGraphPathView draws the path at the top of the editor window.  NSPathControl does most of the heavy lifting.
+// Uniquely, this doesn't have an initWithCoder: initializer, but it _does_ have a _finishInitializing method, so if you serialize this into
+// a nib it'll deserialize and be broken unless you drive _finishInitializing yourself.  This view is apparently only intended to be made
+// programaticaly.
 
 @class GFGraph, GFGraphEditorView, GFNode;
 
@@ -17,7 +21,7 @@
 - (id)rootGraph;
 - (id)currentNode;
 - (void)__graphDidChange:(id)fp8;
-- (void)setPath:(id)fp8;
+- (void)setPath:(NSString*)path;
 - (void)setCurrentNode:(id)fp8;
 - (void)setGraphEditor:(id)fp8;
 - (id)graphEditor;
@@ -30,10 +34,12 @@
 
 @interface GFGraphPathView (Private)
 + (BOOL)_isGraph:(id)fp8;
-- (BOOL)_getPathFromGraph:(id)fp8 toNode:(id)fp12 withBuffer:(id *)fp16;
-- (void)_setCurrentNode:(id)fp8;
-- (id)_setPathWithBuffer:(id *)fp8;
-- (void)_graphUpdated:(id)fp8;
+//- (BOOL)_getPathFromGraph:(id)fp8 toNode:(id)fp12 withBuffer:(id *)fp16; // this is now gone, replaced with the array variant
+- (BOOL)_getPathFromGraph:(id)graph toNode:(id)node withArray:(id)arg3; // added after SSDK was dumped
+- (void)_setCurrentNode:(id)node;
+//- (id)_setPathWithBuffer:(id *)fp8; // this is now gone, replaced with the array variant
+- (id)_setPathWithArray:(NSArray*)pathArray;
+- (void)_graphUpdated:(NSNotification*)notification;
 - (void)__hierarchyControlAction:(id)fp8;
 - (void)_finishInitialization;
 @end

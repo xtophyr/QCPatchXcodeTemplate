2e7aa325b6b975606a7e1a845894b7b6c9722ca0
Christopher Wright <22761542+xtophyr@users.noreply.github.com>
2024-06-13 22:27:56 -0400

    notes for QCRenderState, remove stale offsets.

--- include/QCRenderState.h
+++ include/QCRenderState.h
@@ -3,11 +3,15 @@
 
 @class QCContext, QCPatch;
 
+// This is a lightweight base class that associates a context with a root patch of a composition for rendering.
+// init will throw an exception if either context or patch are nil.  context is retained, patch is not.
+// In normal usage, this is driven as the superclass of QCOpenGLRenderState and QCGraphicsRenderState.
+
 @interface QCRenderState : NSObject
 {
-	QCPatch *_patch;	// 4 = 0x4
-	QCContext *_context;	// 8 = 0x8
-	void *_unused[4];	// 12 = 0xc
+	QCPatch *_patch;
+	QCContext *_context;
+	void *_unused[4];
 }
 
 - (id)initWithContext:(QCContext* _Nonnull)context patch:(QCPatch*_Nonnull)patch;

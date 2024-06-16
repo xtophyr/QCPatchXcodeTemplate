d07e7da6f7865d0bc9128b87bb4ffe946de619b3
Christopher Wright <22761542+xtophyr@users.noreply.github.com>
2024-06-16 17:10:37 -0400

    notes for QCTextView

--- include/QCTextView.h
+++ include/QCTextView.h
@@ -1,3 +1,7 @@
+// This is only used for QCKeyboardUI, the settings panel for the Keyboard patch.
+// under the hood, -keydown: writes to a global bool that's visible to QCKeyboardUI to update the table rows.
+// that seems like an unusual design, with atrocious scoping.
+
 @interface QCTextView : NSTextView
 + (id)sharedInstance;
 - (void)_windowDidResignKeyNotification:(NSNotification*)notification;

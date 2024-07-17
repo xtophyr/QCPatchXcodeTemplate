108d6890d587416f785605544bcc66281e1a922a
Christopher Wright <22761542+xtophyr@users.noreply.github.com>
2024-07-17 14:25:35 -0400

    CGRegion notes

--- include/QCRegion.h
+++ include/QCRegion.h
@@ -1,6 +1,9 @@
 // _region is a quite likely a CGSRegionRef, a scanline-coherent shape object based on "Scanline Coherent Shape Algebra"
 // used mainly in CoreGraphics framework and in the WindowServer/SkyLight.  It's a CF Object.  All SPI there.
 
+// infinite and empty regions are global singletons.
+// It looks like _region is set to point to self for infinite, and points to _bounds for empty, as a sort of sentinel.
+
 @interface QCRegion : NSObject
 {
 	CGRect _bounds;	// 4 = 0x4
@@ -17,24 +20,35 @@
 - (void)finalize;
 - (void)dealloc;
 - (id)initWithCGRect:(CGRect)rect;
-- (CGRect)CGRect;
+- (CGRect)CGRect;   // returns CGNullRect for empty rect
 - (id)initWithCGSRegion:(void *)cgsRegion;
 - (void *)CGSRegion;
 - (id)initWithRect:(NSRect)rect;
-- (NSRect)bounds;
+- (NSRect)bounds;   // bounds is like -CGRect, but will throw for infiniteRegion (rather than returning CGRectInfinite like -CGRect does), and NSZeroRect for empty (not CGNullRect)
 - (BOOL)isEmpty;
 - (BOOL)isInfinite;
 - (BOOL)isRectangular;
 - (BOOL)containsPoint:(NSPoint)point;
 - (BOOL)containsRect:(NSRect)rect;
-- (BOOL)containsRegion:(id)region;
+- (BOOL)containsRegion:(QCRegion*)region;
 - (BOOL)intersectsRect:(NSRect)rect;
-- (BOOL)intersectsRegion:(id)region;
-- (id)listRects;
+- (BOOL)intersectsRegion:(QCRegion*)region;
+- (NSString*)listRects;
 - (id)pixelBufferRepresentation;
-- (void *)enumerateRects:(void *)fp8 outRect:(NSRect *)fp12;
+
+/* to use the enumerator, send in NULL for enumerator, and it'll return a pointer to feed in to subsequent calls, one for each rect. */
+/*
+        void *enumerator = [region enumerateRects:NULL outRect:&rect];
+        if (enumerator)
+            do {
+                // do something with rect
+                enumerator = [region enumerateRects:enumerator outRect:&rect];  // get next rect
+            } while (enumerator);
+ */
+- (void *)enumerateRects:(void *)enumerator outRect:(NSRect *)outRect;
+
 - (id)transformWith:(CGAffineTransform *)tranform;
-- (id)transformBy:(id)fp8;
+- (id)transformBy:(NSAffineTransform*)transform;
 - (id)offsetByX:(CGFloat)dx Y:(CGFloat)dy;
 - (id)scaleByX:(CGFloat)sx Y:(CGFloat)sy;
 - (id)rotateByDegrees:(CGFloat)degrees;
@@ -42,9 +56,9 @@
 - (id)unionWithRect:(NSRect)rect;
 - (id)intersectWithRect:(NSRect)rect;
 - (id)subtractRect:(NSRect)rect;
-- (id)unionWithRegion:(id)region;
-- (id)intersectWithRegion:(id)region;
-- (id)subtractRegion:(id)region;
+- (id)unionWithRegion:(QCRegion*)region;
+- (id)intersectWithRegion:(QCRegion*)region;
+- (id)subtractRegion:(QCRegion*)region;
 - (NSString*)description;
 @end
 

b0e1c4a348113728d7275ea21655fb516ca25120
Christopher Wright <22761542+xtophyr@users.noreply.github.com>
2024-08-23 21:53:21 -0400

    some type info for QCFilter

--- include/QCFilter.h
+++ include/QCFilter.h
@@ -1,16 +1,17 @@
 @class QCImageKernel;
 
+// This class is used for Core Image Filter's Edit Filter Function machinery (need to check the box to instantiate it).
+
 @interface QCFilter : CIFilter
 {
-	
-	JSContextRef _jsContext;	// 36 = 0x24
-	QCImageKernel *_patch;	// 40 = 0x28
+	JSContextRef _jsContext;
+	QCImageKernel *_patch;
 }
 
 - (void)dealloc;
 - (void)finalize;
-- (id)initWithQCImageKernelPatch:(id)fp8;
-- (CGRect)QCImageKernelJavaScriptROIHandler:(int)fp8 destRect:(CGRect)fp12 userInfo:(id)fp28;
+- (id)initWithQCImageKernelPatch:(QCImageKernel*)patch;
+- (CGRect)QCImageKernelJavaScriptROIHandler:(int)fp8 destRect:(CGRect)rect userInfo:(id)info;
 - (void)setROISelectorForKernel:(id)fp8 jsROIFunction:(JSValueRef)fp12;
 - (void)setROIInfoForKernel:(id)fp8 jsROIInfo:(JSValueRef)fp12;
 

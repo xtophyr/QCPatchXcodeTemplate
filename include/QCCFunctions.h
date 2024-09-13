b81a9860d355b9dc0f6c0fc68a87bc5aa01838d9
Christopher Wright <22761542+xtophyr@users.noreply.github.com>
2024-09-12 23:35:40 -0400

    correct this function interface - better types and some notes.

--- include/QCCFunctions.h
+++ include/QCCFunctions.h
@@ -59,7 +59,7 @@ NSData* QCCompositionToData(NSDictionary *compositionDictionary); // serializes 
 void QCCompositionToFile(void);
 void QCCompositionToMovieFile(void);
 int QCCompositionsFromMovieFile(void);  // returns nil (14.4.1)
-CGSize QCComputeAspectRatioBounds(CGSize A, CGSize B, double ratio);
+CGRect QCComputeAspectRatioBounds(CGRect bounds, double ratio); // returns an adjusted rect based on ratio that will fit within bounds.  ratios <0 just return bounds.
 void QCComputeProportionalBounds(void);
 int QCCountCompositionTracksInMovie(void);  // returns 0 (14.4.1)
 void QCDataFromComposition(void); // "deprecated" form of QCCompositionToData
